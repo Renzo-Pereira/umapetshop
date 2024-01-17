4978860960bb206507949e4e2ded01ccbf1cd84d
Renzo Pereira <renzogpereira@gmail.com>
2024-01-17 16:53:16 -0300

    carrusel

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,17 +1,61 @@
 import Item from "./Item";
 import { toCapital } from "../helpers/toCapital";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from 'swiper/modules';
+import "swiper/css";
+import 'swiper/css/pagination';
 
-const ItemList = ( {productos, titulo} ) => {
-
-    return (
-        <div>
-        <h2 className="titulo">{toCapital(titulo)}</h2>
-
-        <div  className="contenedor">
-            { productos.map((prod) => <Item producto={prod} key={prod.id} />) }
-        </div>
+const ItemList = ({ productos, titulo }) => {
+  return (
+    <div className="carrusel">
+      <Swiper
+            modules={[Pagination, Autoplay]}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false
+            }}
+            pagination={{
+              el: ".pagination",
+              clickable: true,
+            }}
+            slidesPerView={4}
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 1,
+                spaceBetween: 25,
+              },
+              "@0.50": {
+                slidesPerView: 1.25,
+                spaceBetween: 25,
+              },
+              "@1.00": {
+                slidesPerView: 2,
+                spaceBetween: 25,
+              },
+              "@1.25": {
+                slidesPerView: 2.5,
+                spaceBetween: 20,
+              },
+              "@1.50": {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+              "@1.75": {
+                slidesPerView: 4,
+                spaceBetween: 20,
+              },
+            }}
+          >
+        {productos.map((prod) => (
+          <SwiperSlide key={prod.id}>
+            {/* Renderiza tu componente de Item con el producto */}
+            <Item producto={prod} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="pagination" />
     </div>
-    )
-}
+  );
+};
 
-export default ItemList
+export default ItemList;
